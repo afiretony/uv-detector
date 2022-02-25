91aef58a4485c93dee9b3948c25fa0a42fbc13c8
Chenhao Yang <yangchenhao@cmu.edu>
2022-02-25 10:20:53 -0500

    bounding boxes visualization

--- uv_detector_node/uv_detector_node.cpp
+++ uv_detector_node/uv_detector_node.cpp
@@ -74,45 +74,133 @@ class my_detector
 			this->uv_detector.extract_3Dbox();
 			this->uv_detector.display_depth();
 
-			visualization_msgs::Marker marker;
-			visualization_msgs::MarkerArray markers;
 			
-			marker.header.frame_id = "/camera_link";
-			marker.id = 0;
-			marker.type = visualization_msgs::Marker::SPHERE;
-			marker.action = visualization_msgs::Marker::ADD;
+// point coordinate
+//											5 _ _ _ 6
+//											| \     | \
+//											|   4 _ _ _ 7
+//											|   |   |   |
+//											|   |   |   |
+//											|   |   |   |
+//											|   |   |   |
+//											|   |   |   |
+//											1 _ | _ 2   |
+//											\ |     \ |
+//												0 _ _ _ 3
+//													^z
+//													|/y
+//													--->x
 
-			double u_r, u_l, d_b, d_t;
-			for(int i = 0; i < this->uv_detector.box3Ds.size(); i++)
-			{
-				cout<<"----------------------------"<<endl;
-				cout<<"Object "<< i <<": "<<endl;
-				cout<<"x: " << uv_detector.box3Ds[i].x<<endl;
-				cout<<"y: " <<uv_detector.box3Ds[i].y<<endl;
-				cout<<"z: " <<uv_detector.box3Ds[i].z<<endl;
+			// visualization using bounding boxes
+			visualization_msgs::Marker line;
+			visualization_msgs::MarkerArray lines;
+			line.header.frame_id = "/camera_link";
+			line.id=0;
+			line.type = visualization_msgs::Marker::LINE_LIST;
+			line.action = visualization_msgs::Marker::ADD;
+			
+			line.scale.x = 0.1;
 
-				marker.lifetime = ros::Duration(0.05);
-				marker.pose.position.x = uv_detector.box3Ds[i].x / 1000.; // convert from mm to m
-				marker.pose.position.y = uv_detector.box3Ds[i].y / 1000.;
-				marker.pose.position.z = uv_detector.box3Ds[i].z / 1000.;
+			// Line list is red
+			line.color.r = 1.0;
+			line.color.a = 1.0;
+			line.lifetime = ros::Duration(0.05);
 
-				marker.scale.x = uv_detector.box3Ds[i].x_width / 1000.;
-				marker.scale.y = uv_detector.box3Ds[i].y_width / 1000.;
-				marker.scale.z = uv_detector.box3Ds[i].z_width / 1000.;
+			for(int i = 0; i < this->uv_detector.box3Ds.size(); i++){
+				float x = uv_detector.box3Ds[i].x / 1000.; // convert from mm to m
+				float y = uv_detector.box3Ds[i].y / 1000.;
+				float z = uv_detector.box3Ds[i].z / 1000.;
 
-				marker.pose.orientation.x = 0.0;
-				marker.pose.orientation.y = 0.0;
-				marker.pose.orientation.z = 0.0;
-				marker.pose.orientation.w = 1.0;
+				float x_width = uv_detector.box3Ds[i].x_width / 1000.; // convert from mm to m
+				float y_width = uv_detector.box3Ds[i].y_width / 1000.;
+				float z_width = uv_detector.box3Ds[i].z_width / 1000.;
+
+				vector<geometry_msgs::Point> verts;
+				geometry_msgs::Point p;
+				// vertice 0
+				p.x = x-x_width / 2.; p.y = y-y_width / 2.; p.z = z-z_width / 2.;
+				verts.push_back(p);
+				// vertice 1
+				p.x = x-x_width / 2.; p.y = y+y_width / 2.; p.z = z-z_width / 2.;
+				verts.push_back(p);
+				// vertice 2
+				p.x = x+x_width / 2.; p.y = y+y_width / 2.; p.z = z-z_width / 2.;
+				verts.push_back(p);
+				// vertice 3
+				p.x = x+x_width / 2.; p.y = y-y_width / 2.; p.z = z-z_width / 2.;
+				verts.push_back(p);
+				// vertice 4
+				p.x = x-x_width / 2.; p.y = y-y_width / 2.; p.z = z+z_width / 2.;
+				verts.push_back(p);
+				// vertice 5
+				p.x = x-x_width / 2.; p.y = y+y_width / 2.; p.z = z+z_width / 2.;
+				verts.push_back(p);
+				// vertice 6
+				p.x = x+x_width / 2.; p.y = y+y_width / 2.; p.z = z+z_width / 2.;
+				verts.push_back(p);
+				// vertice 7
+				p.x = x+x_width / 2.; p.y = y-y_width / 2.; p.z = z+z_width / 2.;
+				verts.push_back(p);
 				
-				marker.color.a = 0.7; // Don't forget to set the alpha!
-				marker.color.r = abs(sin(i));
-				marker.color.g = abs(cos(i));
-				marker.color.b = (abs(cos(i)) + abs(sin(i))) / 2;
-				markers.markers.push_back(marker);
-				marker.id++;
+				
+				
+				line.points.push_back(verts[0]); line.points.push_back(verts[1]);
+				line.points.push_back(verts[1]); line.points.push_back(verts[2]);
+				line.points.push_back(verts[2]); line.points.push_back(verts[3]);
+				line.points.push_back(verts[0]); line.points.push_back(verts[3]);
+				line.points.push_back(verts[0]); line.points.push_back(verts[4]);
+				line.points.push_back(verts[1]); line.points.push_back(verts[5]);
+				line.points.push_back(verts[3]); line.points.push_back(verts[7]);
+				line.points.push_back(verts[2]); line.points.push_back(verts[6]);
+				line.points.push_back(verts[4]); line.points.push_back(verts[5]);
+				line.points.push_back(verts[5]); line.points.push_back(verts[6]);
+				line.points.push_back(verts[4]); line.points.push_back(verts[7]);
+				line.points.push_back(verts[6]); line.points.push_back(verts[7]);
+				
+				lines.markers.push_back(line);
+				line.id++;
 			}
-			marker_pub.publish(markers);
+			marker_pub.publish(lines);
+// 			// visualization using sphere
+			// visualization_msgs::Marker marker;
+			// visualization_msgs::MarkerArray markers;
+			
+			// marker.header.frame_id = "/camera_link";
+			// marker.id = 0;
+			// marker.type = visualization_msgs::Marker::SPHERE;
+			// marker.action = visualization_msgs::Marker::ADD;
+			// double u_r, u_l, d_b, d_t;
+			// for(int i = 0; i < this->uv_detector.box3Ds.size(); i++)
+			// {
+			// 	cout<<"----------------------------"<<endl;
+			// 	cout<<"Object "<< i <<": "<<endl;
+			// 	cout<<"x: " << uv_detector.box3Ds[i].x<<endl;
+			// 	cout<<"y: " <<uv_detector.box3Ds[i].y<<endl;
+			// 	cout<<"z: " <<uv_detector.box3Ds[i].z<<endl;
+
+			// 	marker.lifetime = ros::Duration(0.05);
+			// 	marker.pose.position.x = uv_detector.box3Ds[i].x / 1000.; // convert from mm to m
+			// 	marker.pose.position.y = uv_detector.box3Ds[i].y / 1000.;
+			// 	marker.pose.position.z = uv_detector.box3Ds[i].z / 1000.;
+
+			// 	marker.scale.x = uv_detector.box3Ds[i].x_width / 1000.;
+			// 	marker.scale.y = uv_detector.box3Ds[i].y_width / 1000.;
+			// 	marker.scale.z = uv_detector.box3Ds[i].z_width / 1000.;
+
+			// 	marker.pose.orientation.x = 0.0;
+			// 	marker.pose.orientation.y = 0.0;
+			// 	marker.pose.orientation.z = 0.0;
+			// 	marker.pose.orientation.w = 1.0;
+				
+			// 	marker.color.a = 0.7; // Don't forget to set the alpha!
+			// 	marker.color.r = abs(sin(i));
+			// 	marker.color.g = abs(cos(i));
+			// 	marker.color.b = (abs(cos(i)) + abs(sin(i))) / 2;
+			// 	markers.markers.push_back(marker);
+			// 	marker.id++;
+			// }
+
+			// marker_pub.publish(markers);
 		}
 
 	private:  
